ee3eb505efa1a9f62094148474279860ac8210bf
tuanta7 <anhtuan9702@gmail.com>
2026-07-13 00:00:06 +0700

    chore: update tailwind classname

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -20,7 +20,7 @@ export function Switch({
     >
       <span
         className={`inline-block h-5 w-5 transform rounded-full bg-white shadow transition-transform duration-200 ${
-          checked ? "translate-x-[22px]" : "translate-x-0.5"
+          checked ? "translate-x-5.5" : "translate-x-0.5"
         }`}
       />
     </button>
